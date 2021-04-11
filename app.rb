d4fde03642dd38fb117545ce57b3fa3f1a36322e
DAngelo Thomas <dangelo1thomas@gmail.com>
2021-04-10 23:39:13 -0400

    commit tree 6dd2b5af741b0a2091003f71620ca4908e3b5da3

--- app.rb
+++ app.rb
@@ -17,12 +17,45 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    v = params[:number]
+    v = params[:number].to_i
     v_two = params[:phrase]
-   binding.pry
-   v.times {puts "#{v_two}"}
-  #  "#{v.times(v_two)} #{v_two}"
+  #  binding.pry
+  count = ""
+   v.times do 
+      count += v_two
+   end
+   count
+  
+  end
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    v = params[:word1]
+    v2 = params[:word2]
+    v3 = params[:word3]
+    v4 = params[:word4]
+    v5 = params[:word5]
+    b = " "
+    c = "."
+   v + b + v2 + b + v3+ b + v4 + b + v5 + c
+  end
 
+  get '/:operation/:number1/:number2' do 
+    oper = params[:operation]
+    num = params[:number1].to_i
+    num2 = params[:number2].to_i
+    # binding.pry
+    if oper == "add"
+      a = num + num2
+      a.to_s
+    elsif oper == "subtract"
+      s = num - num2
+      s.to_s
+    elsif oper == "multiply"
+      m = num * num2
+      m.to_s
+    else oper == "divide"
+      d = num / num2
+      d.to_s
+    end
   end
 
 end
