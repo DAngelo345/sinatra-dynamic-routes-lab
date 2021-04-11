d9af737a179a6a13370e70875d6517ee707a4187
DAngelo Thomas <dangelo1thomas@gmail.com>
2021-04-10 22:21:22 -0400

    Done.

--- app.rb
+++ app.rb
@@ -9,11 +9,20 @@ class App < Sinatra::Base
   end
 
   get '/square/:number' do 
+    v = params[:number].to_i
+    
+    new_v = v ** 2
+    new_v.to_s
+    
+  end
+
+  get '/say/:number/:phrase' do
     v = params[:number]
-  
-    v ** 2
-    resp.status = 200
-    resp.finish
+    v_two = params[:phrase]
+   binding.pry
+   v.times {puts "#{v_two}"}
+  #  "#{v.times(v_two)} #{v_two}"
+
   end
 
 end
